e3367cf8383ccfce6fd074b53d5abe760c28bfc3
Faholan <62927863+Faholan@users.noreply.github.com>
2021-01-14 11:50:43 +0100

    Rename Chaotic Bot to bot I don't know why I postponed that for so long

---
+++ startup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Warning : this script is completely untested.
+# It theorically works, but hasn't been tested yet.
+while ! /bin/ss -H -t -l -n sport = :2333 | /bin/grep -q "^LISTEN.*:2333";
+do /bin/sleep 1;
+done
+/usr/bin/python3 -m bot
