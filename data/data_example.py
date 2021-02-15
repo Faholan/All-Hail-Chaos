ddebf24c3c4f8855133b9bc59d8241e5bd954576
Faholan <62927863+Faholan@users.noreply.github.com>
2021-02-15 17:40:30 +0100

    Update License

--- data/data_example.py
+++ data/data_example.py
@@ -1,8 +1,8 @@
 """Rename this file to data.py.
 
-MIT License
+MIT License.
 
-Copyright (c) 2020 Faholan
+Copyright (c) 2020-2021 Faholan
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
