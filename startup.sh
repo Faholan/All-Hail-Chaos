f873c9471462cd4f7734473002411d54b70d8b7a
Faholan <62927863+Faholan@users.noreply.github.com>
2021-03-07 20:39:04 +0100

    Fix systemd startup

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 # It theorically works, but hasn't been tested yet.
 while ! /bin/ss -H -t -l -n sport = :2333 | /bin/grep -q "^LISTEN.*:2333";
 do /bin/sleep 1;
-done
-/usr/bin/python3 -m bot
+done;
+/usr/bin/python3 -m bot;
