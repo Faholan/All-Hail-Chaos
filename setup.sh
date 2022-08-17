61a94b3d1e3c7b4599bd459ff680bcef9a0530f2
Waylander <62927863+Faholan@users.noreply.github.com>
2022-08-17 14:27:56 +0200

    Fix database & setup scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 python3 -m pip install -U virtualenv
 python3 -m virtualenv venv
 source venv/bin/activate
-
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 deactivate
