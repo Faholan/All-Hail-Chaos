666b322cd8a4638f9e8462ceba52f5a835a84b4c
Waylander <62927863+Faholan@users.noreply.github.com>
2022-08-18 22:10:20 +0200

    revert fix

--- startup.sh
+++ startup.sh
@@ -2,5 +2,4 @@
 while ! /bin/ss -H -t -l -n sport = :2333 | /bin/grep -q "^LISTEN.*:2333";
 do /bin/sleep 1;
 done;
-export PATH=$HOME/.local/.bin:$PATH;  # Add local bin to PATH - required for pipx-installed poetry
 poetry run python -m bot;
