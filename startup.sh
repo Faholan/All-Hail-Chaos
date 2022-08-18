b9c819352575afeefdcdaf2fbaea83d725700c0b
Waylander <62927863+Faholan@users.noreply.github.com>
2022-08-18 22:04:24 +0200

    Fix startup script issue

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 while ! /bin/ss -H -t -l -n sport = :2333 | /bin/grep -q "^LISTEN.*:2333";
 do /bin/sleep 1;
 done;
+export PATH=$HOME/.local/.bin:$PATH;  # Add local bin to PATH - required for pipx-installed poetry
 poetry run python -m bot;
