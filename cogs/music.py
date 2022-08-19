5e6cc594f9497a9e6ecfece8f819b4e1a8a36c29
Solteneo <69587030+Solteneo@users.noreply.github.com>
2022-08-19 15:24:47 +0200

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -389,17 +389,15 @@ async def get_music_embed(
 
     for i in range(min(len(player.history), 5)):
         mus = player.history[-i]
-        desc += "[-" + duration_str(mus.duration)
-        desc += " : " + mus.title
-        desc += f'](https://www.youtube.com/watch?v={mus.identifier})"{mus.title}"'
+        desc += "-[" + mus.title
+        desc += f'](https://www.youtube.com/watch?v={mus.identifier}) "{mus.title}\n"'+f" ({duration_str(mus.duration)})"
     if player.queue:
         desc += "\n\nNext :\n"
 
     for i in range(min(len(player.queue), 5)):
         mus = player.queue[i]
-        desc += "[-" + duration_str(mus.duration)
-        desc += " : " + mus.title
-        desc += f'](https://www.youtube.com/watch?v={mus.identifier})"{mus.title}"'
+        desc += "-[" + mus.title
+        desc += f'](https://www.youtube.com/watch?v={mus.identifier}) "{mus.title}\n"'+f" ({duration_str(mus.duration)})"
     if player.current:
         mus: lavalink.AudioTrack = player.current
         embed = discord.Embed(title=f"Currently playing:{duration_str(mus.duration)} : "+ mus.title, description=desc,url="https://www.youtube.com/watch?v="+player.current.identifier)
